8db3bba0f9efdb2cb21ccea0b14b62d23c925539
Jamie Strausbaugh <jstrausb86@gmail.com>
2018-09-20 16:53:59 -0400

    Added key for MapBox API to initMap() in both main.js and restaurant_info.js

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoiamV0YWxzIiwiYSI6ImNqbWF5ZnQ4cTVhN2Izd2xpNHN3b3R2MDkifQ.yFJp5JU0G8CSAgYzz0rIfg',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
