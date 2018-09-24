3a4ac9e4ec904a469389acc21627789bf430f403
Jamie Strausbaugh <jstrausb86@gmail.com>
2018-09-24 15:34:35 -0400

    Modified sw.js to improve cache handling and returning responses from fetch requests.

--- sw.js
+++ sw.js
@@ -1,5 +1,8 @@
-const staticCacheName = 'rest-reviews-static-v1';
+const staticCacheName = 'rest-reviews-cache-v3';
 
+// After registration, callback for install event should:
+// 1. open / create custom cache if it does not exist
+// 2. add page resources to cache by passing array into addAll()
 self.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open(staticCacheName).then(function(cache) {
@@ -27,31 +30,57 @@ self.addEventListener('install', function(event) {
     );
 });
 
+self.addEventListener('fetch', function(event) {
+    // TODO: respond with an entry from the cache if there is one.
+    // If there isn't, fetch from the network.
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            // if match in cache exists, return response
+            if (response) {
+                return response;
+            }
 
+            /* Begin Attribution
+                Referenced Matt Gaunt's article: Service Workers: an Introduction
+                for support in setting up response clone in sw's fetch event:
+                https://developers.google.com/web/fundamentals/primers/service-workers/
+                on September 24, 2018
+            End Attribution */
 
+            // Begin reference to helper code abstraction
+            
+            /*
+            No match in cache? Clone the request.
+            Then we will clone the response,
+            providing two response objects;
+            one to send to browser to load,
+            and the other to send to the cache for storage.
+            */
+            let fetchRequest = event.request.clone();
 
+            // return result of call to fetch, retrieving request from network
+            return fetch(fetchRequest).then(
+                function(response) {
+                    // check state of response
+                    if(!response || response.type !== 200 || response.type !== 'basic') {
+                        return response;
+                    }
 
-// self.addEventListener('fetch', function(event) {
-//     // TODO: respond with an entry from the cache if there is one.
-//     // If there isn't, fetch from the network.
-//     event.respondWith(
-//         caches.match(event.request).then(function(response) {
-//             if (response) {
-//                 console.log('Found ', event.request, ' in cache');
-//                 return response;
-//             }
-//             else {
-//                 console.log('Unable to retrieve ', event.request, ' in cache. Fetching now.');
-//                 return fetch(event.request).then(function(response) {
-//                     const clonedResponse = response.clone();
-//                     caches.open(staticCacheName).then(function(cache) {
-//                         cache.put(event.request, clonedResponse);
-//                     })
-//                     return response;
-//                 }).catch(function(err) {
-//                     console.error(err);
-//                 });
-//             }
-//         })
-//     );
-// });
+                    // Having passed the checks, now clone the response.
+                    // Save clone in variable for storing in cache.
+                    // Return original response to browser for consumption.
+                    let responseToCache = response.clone();
+
+                    // saving cloned response in cache
+                    caches.open(staticCacheName).then(function(cache) {
+                        cache.put(event.request, responseToCache);
+                    });
+
+                    return response;
+                }
+            )
+            // End reference to helper code abstraction
+        })
+    );
+});
+
